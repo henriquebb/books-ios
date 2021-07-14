9cd78d30ce0da2f4f7e9f9f9735e7f3831047186
Henrique Barbosa <henriqbarbosa@icloud.com>
2021-07-14 11:30:21 -0300

    Decouple login vc from custom views

--- books-ios/books-ios/Views/Controllers/LoginViewController.swift
+++ books-ios/books-ios/Views/Controllers/LoginViewController.swift
@@ -14,11 +14,8 @@ protocol LoginPresenting: AnyObject {
 
 class LoginViewController: UIViewController {
 
-    lazy var textFieldStack = TextFieldStack()
-    lazy var titleStack = TitleStack()
-    lazy var imageView = BackgroundImage()
     var presenter: LoginPresenter?
-    private var orientationIsLandscape: Bool?
+    var loginView: LoginView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,88 +24,35 @@ class LoginViewController: UIViewController {
 
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
-        orientationIsLandscape = UIDevice.current.orientation.isLandscape
-        setBackgroundImageBasedOnOrientation()
+        loginView?.delegate?.setBackgroundImageBasedOnOrientation()
     }
 }
 
 // MARK: - Setup
 
 extension LoginViewController {
-    private func addSubviews() {
-        textFieldStack.tag = Tags.textField.rawValue
-        [imageView, textFieldStack, titleStack].forEach { (subview: ViewConstraintsDelegate) in
-            view.addSubview(subview as? UIView ?? UIView())
-            subview.setConstraints()
-        }
+
+    override func loadView() {
+        super.loadView()
+        loginView = LoginView()
+        view = loginView
     }
 
     private func setup() {
-        orientationIsLandscape = UIDevice.current.orientation.isLandscape
-        addKeyboardObservers()
         presenter?.attachView(view: self)
-        setBackgroundImageBasedOnOrientation()
-        addSubviews()
     }
 }
 
 // MARK: - LoginPresenting
 
 extension LoginViewController: LoginPresenting {
-    func sendInputText() {
-        let emailTextField = textFieldStack.email.subviews
-            .filter { $0.tag == Tags.textField.rawValue }.first as? UITextField
-        let passwordTextField = textFieldStack.password.subviews
-            .filter { $0.tag == Tags.textField.rawValue }.first as? UITextField
-        presenter?.signIn(email: emailTextField?.text ?? "",
-                         password: passwordTextField?.text ?? "")
-    }
-
     func didTapEnter() {
         sendInputText()
     }
-}
 
-// MARK: - Keyboard
-
-extension LoginViewController {
-    private func addKeyboardObservers() {
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillShow),
-                                               name: UIResponder.keyboardWillShowNotification,
-                                               object: nil)
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillHide),
-                                               name: UIResponder.keyboardWillHideNotification,
-                                               object: nil)
-    }
-
-    @objc private func keyboardWillShow(notification: NSNotification) {
-        if orientationIsLandscape ?? false {
-            textFieldStack.stackCenterYConstraint?.constant = -80
-            titleStack.isHidden = true
-            UIView.animate(withDuration: 0.35) {
-                self.view.layoutIfNeeded()
-            }
-        }
-    }
-
-    @objc private func keyboardWillHide(notification: NSNotification) {
-        if orientationIsLandscape ?? false {
-            textFieldStack.stackCenterYConstraint?.constant = 0
-            titleStack.isHidden = false
-            UIView.animate(withDuration: 0.35) {
-                self.view.layoutIfNeeded()
-            }
-        }
-    }
-}
-
-// MARK: - Background Image
-
-extension LoginViewController {
-    private func setBackgroundImageBasedOnOrientation() {
-        imageView.image = orientationIsLandscape ?? false ?
-            UIImage(named: "splashBackgroundLandscape") : UIImage(named: "splashBackground")
+    func sendInputText() {
+        let inputValues = loginView?.delegate?.getTextFieldValues()
+        presenter?.signIn(email: inputValues?.first ?? "",
+                          password: inputValues?.last ?? "")
     }
 }
---
+++ books-ios/books-ios/Views/Custom/Login/LoginView.swift
@@ -0,0 +1,114 @@
+//
+//  LoginView.swift
+//  books-ios
+//
+//  Created by Henrique Barbosa on 13/07/21.
+//
+
+import UIKit
+
+protocol LoginViewDelegate: AnyObject {
+    func setBackgroundImageBasedOnOrientation()
+    func getTextFieldValues() -> [String]
+}
+
+class LoginView: UIView {
+
+    lazy private var textFieldStack = TextFieldStack()
+    lazy private var titleStack = TitleStack()
+    lazy private var imageView = BackgroundImage()
+    weak var delegate: LoginViewDelegate?
+    private var orientationIsLandscape: Bool?
+
+    init() {
+        super.init(frame: CGRect())
+        setup()
+
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Setup
+
+extension LoginView {
+
+    private func setup() {
+        addTagToSubviews()
+        addSubviews()
+        setBackgroundImageBasedOnOrientation()
+        setDelegate()
+        addKeyboardObservers()
+    }
+
+    private func setDelegate() {
+        delegate = self
+    }
+
+    private func addTagToSubviews() {
+        textFieldStack.tag = Tags.textField.rawValue
+    }
+
+    private func addSubviews() {
+        [imageView, textFieldStack, titleStack].forEach { (subview: ViewConstraintsDelegate) in
+            addSubview(subview as? UIView ?? UIView())
+            subview.setConstraints()
+        }
+    }
+}
+
+// MARK: - Keyboard
+
+extension LoginView {
+    private func addKeyboardObservers() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        if orientationIsLandscape ?? false {
+            textFieldStack.stackCenterYConstraint?.constant = -80
+            titleStack.isHidden = true
+            UIView.animate(withDuration: 0.35) {
+                self.layoutIfNeeded()
+            }
+        }
+    }
+
+    @objc private func keyboardWillHide(notification: NSNotification) {
+        if orientationIsLandscape ?? false {
+            textFieldStack.stackCenterYConstraint?.constant = 0
+            titleStack.isHidden = false
+            UIView.animate(withDuration: 0.35) {
+                self.layoutIfNeeded()
+            }
+        }
+    }
+}
+
+// MARK: - LoginViewDelegate
+
+extension LoginView: LoginViewDelegate {
+
+    func setBackgroundImageBasedOnOrientation() {
+        orientationIsLandscape = UIDevice.current.orientation.isLandscape
+        imageView.image = orientationIsLandscape ?? false ?
+            UIImage(named: "splashBackgroundLandscape") : UIImage(named: "splashBackground")
+    }
+
+    func getTextFieldValues() -> [String] {
+        let emailTextField = textFieldStack.email.subviews
+            .filter { $0.tag == Tags.textField.rawValue }.first as? UITextField
+        let passwordTextField = textFieldStack.password.subviews
+            .filter { $0.tag == Tags.textField.rawValue }.first as? UITextField
+        return [emailTextField?.text ?? "", passwordTextField?.text ?? ""]
+    }
+}
