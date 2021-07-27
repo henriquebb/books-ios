b0292f1f1a828283b769b12c71b55196a777d0d6
Henrique Barbosa <henriqbarbosa@icloud.com>
2021-07-27 10:08:35 -0300

    Add details-screen components

--- books-ios/books-ios/Views/Custom/Details/DetailsView.swift
+++ books-ios/books-ios/Views/Custom/Details/DetailsView.swift
@@ -11,7 +11,174 @@ class DetailsView: UIView {
 
     // MARK: - Views
 
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+
     private lazy var exitXButton = ExitXButton()
+
+    // MARK: - Top Content
+
+    private lazy var imageView: UIView = {
+        let view = UIView()
+        let imageView = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "book_1")
+        view.addSubview(imageView)
+        setImageSize(image: imageView)
+        setViewHeight(view: view)
+        setImageViewConstraints(view: imageView, superview: view)
+        return view
+    }()
+
+    private lazy var title: UIStackView = {
+        let stack = UIStackView()
+        let label = UILabel()
+        label.text = "LabelLabelLabelLabelLabelLabelLabelLabel"
+        label.numberOfLines = 0
+        stack.addArrangedSubview(label)
+        return stack
+    }()
+
+    private lazy var authors: UIStackView = {
+        let stack = UIStackView()
+        let label = UILabel()
+        label.text = "LabelLabelLabelLabelLabelLabelLabelLabel"
+        label.numberOfLines = 0
+        stack.addArrangedSubview(label)
+        return stack
+    }()
+
+    private lazy var topStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        [title, authors].forEach { stack.addArrangedSubview($0) }
+        return stack
+    }()
+
+    // MARK: - Middle Content
+
+    private lazy var middleStackTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Informações"
+        return label
+    }()
+
+    private lazy var middleStackLeftContent: UIStackView = {
+        let stack = UIStackView()
+        ["Páginas", "Editora", "Publicação",
+         "Idioma", "Título Original", "ISBN-10", "ISBN-13"].forEach {
+            let label = UILabel()
+            label.text = $0
+            stack.addArrangedSubview(label)
+         }
+        stack.axis = .vertical
+        return stack
+    }()
+
+    private lazy var pages: UILabel = {
+        let label = UILabel()
+        label.text = "Label"
+        return label
+    }()
+
+    private lazy var publisher: UILabel = {
+        let label = UILabel()
+        label.text = "Label"
+        return label
+    }()
+
+    private lazy var publishingDate: UILabel = {
+        let label = UILabel()
+        label.text = "Label"
+        return label
+    }()
+
+    private lazy var language: UILabel = {
+        let label = UILabel()
+        label.text = "Label"
+        return label
+    }()
+
+    private lazy var originalTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Label"
+        return label
+    }()
+
+    private lazy var isbn10: UILabel = {
+        let label = UILabel()
+        label.text = "Label"
+        return label
+    }()
+
+    private lazy var isbn13: UILabel = {
+        let label = UILabel()
+        label.text = "Label"
+        return label
+    }()
+
+    private lazy var middleStackRightContent: UIStackView = {
+        let stack = UIStackView()
+        [pages, publisher, publishingDate,
+         language, originalTitle, isbn10, isbn13].forEach { stack.addArrangedSubview($0) }
+        stack.axis = .vertical
+        stack.alignment = .trailing
+        return stack
+    }()
+
+    private lazy var middleStackBottomContent: UIStackView = {
+        let stack = UIStackView()
+        [middleStackLeftContent, middleStackRightContent].forEach { stack.addArrangedSubview($0) }
+        return stack
+    }()
+
+    private lazy var middleStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 17
+        [middleStackTitle, middleStackBottomContent].forEach { stack.addArrangedSubview($0) }
+        return stack
+    }()
+
+    // MARK: - Bottom Content
+
+    private lazy var bottomStackTitle: UILabel = {
+        let label = UILabel()
+        label.text = "RESENHA DA EDITORA"
+        return label
+    }()
+
+    private lazy var bottomStackBottom: UIStackView = {
+        let stack = UIStackView()
+        let label = UILabel()
+        label.text = "LabelLabelLabelLabelLabelLabelLabelLabelLabelLabelLabelLabelLabel"
+        label.numberOfLines = 0
+        stack.addArrangedSubview(label)
+        return stack
+    }()
+
+    private lazy var bottomStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 17
+        [bottomStackTitle, bottomStackBottom].forEach { stack.addArrangedSubview($0) }
+        return stack
+    }()
+
+    // MARK: - Main Content
+
+    private lazy var mainStack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.spacing = 32
+        [imageView, topStack, middleStack, bottomStack].forEach { stack.addArrangedSubview($0) }
+        return stack
+    }()
 
     // MARK: - Init
 
@@ -28,11 +195,61 @@ class DetailsView: UIView {
 // MARK: - Setup
 
 extension DetailsView {
-    func setup() {
+    private func setup() {
         backgroundColor = .white
         layer.cornerRadius = 4
         translatesAutoresizingMaskIntoConstraints = false
-        addSubview(exitXButton)
+        scrollView.addSubview(mainStack)
+        [exitXButton, scrollView].forEach { addSubview($0) }
+        setConstraints()
+    }
+    
+    private func setConstraints() {
         exitXButton.setConstraints(type: nil)
+        setConstraints(view: scrollView)
+        setMainStackConstraints()
+        setScrollViewContentLayoutConstraints()
+    }
+}
+
+// MARK: - Size
+
+extension DetailsView {
+    private func setImageSize(image: UIImageView) {
+        NSLayoutConstraint.activate([image.heightAnchor.constraint(equalToConstant: 351),
+                                     image.widthAnchor.constraint(equalToConstant: 240)])
+    }
+
+    private func setViewHeight(view: UIView) {
+        NSLayoutConstraint.activate([view.heightAnchor.constraint(equalToConstant: 351)])
+    }
+}
+
+// MARK: - Constraints
+
+extension DetailsView {
+    private func setMainStackConstraints() {
+        NSLayoutConstraint.activate([mainStack.topAnchor
+                                        .constraint(equalTo: scrollView.contentLayoutGuide.topAnchor,
+                                                    constant: 24),
+                                     mainStack.bottomAnchor
+                                        .constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor,
+                                                    constant: -32),
+                                     mainStack.leadingAnchor
+                                        .constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor,
+                                                    constant: 24),
+                                     mainStack.trailingAnchor
+                                        .constraint(equalTo: scrollView.frameLayoutGuide.trailingAnchor,
+                                                    constant: -24)])
+    }
+    
+    private func setScrollViewContentLayoutConstraints() {
+        NSLayoutConstraint.activate([scrollView.contentLayoutGuide.trailingAnchor
+                                        .constraint(equalTo: mainStack.trailingAnchor, constant: 24)])
+    }
+
+    private func setImageViewConstraints(view: UIView, superview: UIView) {
+        NSLayoutConstraint.activate([view.centerXAnchor.constraint(equalTo: superview.centerXAnchor),
+                                     view.centerYAnchor.constraint(equalTo: superview.centerYAnchor)])
     }
 }
